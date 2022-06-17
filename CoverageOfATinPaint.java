2c17c3bcd731eb3ecde487477d770466131d3334
rihan97 <rihan.alam97@gmail.com>
2022-06-17 12:14:30 +0100

    added code and readme file

---
+++ CoverageOfATinPaint.java
@@ -0,0 +1,63 @@
+package uk.ac.mmu.cnt.one;
+
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	
+public class CoverageOfATinPaint {
+
+	public static void main(String[] args) throws IOException{
+		
+		//declare some variables
+		double height;
+		double width;
+		double area;
+		double tinOfPaint;
+		double numberOfTins; 
+		
+		String heightStr;
+		String widthStr;
+		String areaStr;
+		String tinOfPaintStr;
+		
+		
+		//set up a reader form the console window
+		BufferedReader console =
+				new BufferedReader (new InputStreamReader(System.in));
+		
+		//read in values interactively from the command line
+		System.out.println("Please enter a value for the height: ");
+		heightStr = console.readLine();
+	    height = Double.parseDouble(heightStr);
+		
+		System.out.println("Please enter a value for the width: ");
+		widthStr = console.readLine();
+		width = Double.parseDouble(widthStr);
+		
+		System.out.println("Please enter a value for the area: ");
+		areaStr = console.readLine();
+		area = Double.parseDouble(areaStr);
+		
+		System.out.println("Please enter a value for the coverage of a tin of paint: ");
+		tinOfPaintStr = console.readLine();
+		tinOfPaint = Double.parseDouble(tinOfPaintStr);
+				
+		
+		//for debugging we want to display our values to make sure everything is ok
+		System.out.println("We have: ");
+		System.out.println("Height value = " + height);
+		System.out.println("Width value = " + width);
+		System.out.println("Area value = " + area);
+		System.out.println("Tin of paint value = " + tinOfPaint);
+
+		
+		//Calculate our number of tins value
+		area = height * width; 
+		numberOfTins = area / tinOfPaint;
+		
+		//Display  the value
+		System.out.println("The number of tins needed for a given wall is " + numberOfTins + " Metres2");
+				
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+### ***Calculate the number of tins needed for a given wall***
+
+The Number of tins calulator program was developed to calculate how many tins are needed to paint a wall, once you input the values for the following; height value, width value, area value and tin of paint value it the calculated the result. 
+
+ There were two lines of formulas used; the first one was ‘area = height * width’ and ‘number of tins = area / tin of paint’.  The following is some test data you can use to test the program.
+
+| Height Value | Width Value | Area Value | Coverage of Paint Value | Program results          |
+| ------------ | ----------- | ---------- | ----------------------- | ------------------------ |
+| 10           | 20          | 30         | 40                      | 5 metres (squared)       |
+| 5            | 10          | 15         | 20                      | 2.5 metres (squared)     |
+| 25.7         | 18.34       | 3.2        | 1.8                     | 261.854 metres (squared) |
+| 50           | 78          | 43         | 2                       | 195 metres (squared)     |
+| 13           | 12          | 11         | 9                       | 17.3 metres (squared)    |
+| 8            | 17          | 25         | 19                      | 7.5 metres (squared)     |
+| 34           | 45          | 65         | 2                       | 765 metres (squared)     |
+
+
+Feel free to use your own test data to have a go at calculating the supply voltage.
+
